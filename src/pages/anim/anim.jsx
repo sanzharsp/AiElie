cc34a59916e310a6ebee2b9fb64a26e40670a6d6
sanzharsp <sanzharsapar1@gmail.com>
2023-05-07 01:06:37 +0600

    Photos

--- src/pages/anim/anim.jsx
+++ src/pages/anim/anim.jsx
@@ -2,9 +2,9 @@
 import './styles.css'
 import React, { useState } from 'react'
 import { useTrail, a } from '@react-spring/web'
-    
+import { Avatar } from 'antd';
+import url from '../../assets/ava.png'
 
-    
 const Trail = ({ open, children }) => {
       const items = React.Children.toArray(children);
   
@@ -25,20 +25,50 @@ const Trail = ({ open, children }) => {
         </div>
       )
     }
+
+    const Trail2 = ({ open, children }) => {
+      const items = React.Children.toArray(children);
+  
+      const trail = useTrail(items.length, {
+        config: { mass: 5, tension: 2000, friction: 200 },
+        opacity: open ? 1 : 0,
+        x: open ? 0 : 20,
+        height: open ? 110 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+      })
+      return (
+        <div>
+          {trail.map(({ height, ...style }, index) => (
+            <a.div key={index} className="trailsText2" style={style}>
+              <a.div style={{ height }}>{items[index]}</a.div>
+            </a.div>
+          ))}
+        </div>
+      )
+    }
     
+
 export default function Animations() {
       const [open, set] = useState(true)
       setTimeout(()=>set(!open), 4000);
       return (
-        
+        <>
+    
         <div className="container" >
+          <div style={{padding:20}}>
+        <Trail2 open={open}>
+        <Avatar className="avatar" src={<img src={url}  />} />
+        </Trail2>
+        </div>
           <Trail open={open}>
             <span style={{color:'red'}}>CREATED</span>
             <span >BY</span>
             <span style={{color:'red'}}>ELIZAT</span>
             <span>DAULETOVA</span>
           </Trail>
+   
         </div>
+        </>
       )
     }
     
